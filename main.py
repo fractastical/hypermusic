73edfdb52d3a3f09e8d72a82f647fd13763276af
Joel Dietz <jdietz+github@gmail.com>
2023-07-24 12:06:43 +0200

    semf success

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import networkx as nx
-from music21 import stream, note, midi
+from music21 import stream, note, chord, midi
 
-# Create a hypergraph using a dictionary. The key is the hyperedge (chord), 
+# Create a hypergraph using a dictionary. The key is the hyperedge (chord name), 
 # and the value is a list of nodes (notes).
 hypergraph = {
     "chord1": ["C4", "E4", "G4"],  # C Major chord
-    "chord2": ["A4", "C5", "E5"]  # A minor chord
+    "chord2": ["A4", "C5", "E5"]   # A minor chord
 }
 
 # Convert the hypergraph to a NetworkX bipartite graph
 B = nx.Graph()
-for chord, notes in hypergraph.items():
-    for note in notes:
-        B.add_edge(chord, note)
+for chord_name, notes in hypergraph.items():
+    for note_name in notes:
+        B.add_edge(chord_name, note_name)
 
 # Verify that the graph is bipartite
 assert nx.is_bipartite(B)
@@ -21,12 +21,13 @@ assert nx.is_bipartite(B)
 s = stream.Stream()
 
 # Add each chord to the stream
-for chord in hypergraph.keys():
+for chord_name in hypergraph.keys():
     # Get the notes of this chord
-    notes = [note.Note(n) for n in B.neighbors(chord)]
-    # Add the notes to the stream
-    for n in notes:
-        s.append(n)
+    chord_notes = [note.Note(n) for n in B.neighbors(chord_name)]
+    # Create a chord from these notes
+    chord_to_add = chord.Chord(chord_notes)
+    # Add the chord to the stream
+    s.append(chord_to_add)
 
 # Write the stream to a MIDI file
 mf = midi.translate.streamToMidiFile(s)
